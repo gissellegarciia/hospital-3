9195b69f6e587073de06048574def6dc4976e784
gissellegarciia <gisellegl@msmk.university>
2025-02-15 14:11:30 +0100

    Agregar archivos de proyecto.

---
+++ hospital-3.cpp
@@ -0,0 +1,12 @@
+﻿// hospital-3.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "hospital-3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
